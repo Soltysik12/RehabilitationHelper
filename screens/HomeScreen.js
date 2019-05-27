ee245b75401a604960b7b8f8440c7ac09ec5b9c7
Agnieszka Sołtysik <asoltysik96@gmail.com>
2019-05-27 20:12:42 +0200

    Init project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import {WebBrowser} from 'expo';
+
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        header: null,
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.tabBarInfoContainer}>
+                    <Text style={styles.tabBarInfoText}>The best app</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {height: -3},
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
+});
