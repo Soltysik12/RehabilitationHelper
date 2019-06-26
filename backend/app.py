2f36ce74386d898e9274561ce0aab62c4730f399
imthumbelina <aga.zmc@gmail.com>
2019-06-26 18:24:51 +0200

    Add returning results from api

--- backend/app.py
+++ backend/app.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+	return "Hello, World!"
 
 @app.route("/api/sensor", methods=["POST"])
 def json_example():
-    req = request.get_json()
-    x = req['data']
-    print(x)
-    if(len(x)):
-        print(find_repetitions_and_exercise(x))
-    return "Thanks!"
+	req = request.get_json()
+	x = req['data']
+	print(x)
+	if(len(x)):
+		json_data = find_repetitions_and_exercise(x)
+		print(json_data)
+		return json_data
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
